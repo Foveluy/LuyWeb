4f998f4edfae0bd9c83caae2d6585960312d1502
方正 <215566435@qq.com>
2017-11-19 00:26:27 +1300

    🌟新增蓝图测试

--- blueprint_test.py
+++ blueprint_test.py
@@ -33,6 +33,31 @@ class TestRouter(unittest.TestCase):
 
         self.assertEqual(kw, {'tag': 1234})
         self.assertEqual(handler, self.noop)
+    
+    def test_muilt_Blueprint(self):
+        
+        request = request_class(url='/bp/1234-')
+        router_instance = Router()
+        router_instance.set_url('/<tag>', self.noop)
+        router_instance.set_url('/bp/<tag:int>', self.noop)
+        router_instance.set_url('/bp2/<tag:int>', self.noop)
+        
+        with self.assertRaises(LuyAException):
+            router_instance.get_mapped_handle(request)
+        
+        request = request_class(url='/bp/1234')
+        handler, kw = router_instance.get_mapped_handle(request)
+
+        self.assertEqual(kw, {'tag': 1234})
+        self.assertEqual(handler, self.noop)
+
+        request = request_class(url='/bp2/1234')
+        handler, kw = router_instance.get_mapped_handle(request)
+
+        self.assertEqual(kw, {'tag': 1234})
+        self.assertEqual(handler, self.noop)
+
+
 
     def noop(self):
         pass
